9d837d606dbda33c5215532f836de9c020d33ff7
Andre Jacobs <andrejacobs@users.noreply.github.com>
2025-09-20 15:58:58 +0100

    wip. Writing the walked files to a file so we can do a diff

--- Planning/benchmarks/walk/bench_walk_test.go
+++ Planning/benchmarks/walk/bench_walk_test.go
@@ -1,6 +1,9 @@
 package walk_test
 
 // Run with: BENCH_DIR="./path/to/dataset" go test -run=^$ -bench=. -benchtime=3x
+// Optionally BENCH_OUT="~/temp/output"
+
+// Reference:
 // -benchtime=10x Limits iterations to 10 times
 // -benchtime=10s Limits iteration time to 10 seconds
 
@@ -8,6 +11,7 @@ import (
 	"fmt"
 	"io/fs"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/andrejacobs/go-aj/file"
@@ -15,14 +19,26 @@ import (
 
 const (
 	benchDirEnv = "BENCH_DIR"
+	outDirEnv   = "BENCH_OUT"
 )
 
 var benchDir string
+var outDir string
 
 func setup() error {
 	benchDir = os.Getenv(benchDirEnv)
 	if benchDir == "" {
 		return fmt.Errorf("environment variable %q needs to specify that directory to be used in the benchmark", benchDirEnv)
+	}
+
+	outDir = os.Getenv(outDirEnv)
+	if outDir == "" {
+		outDir = os.TempDir()
+	}
+	var err error
+	outDir, err = file.ExpandPath(outDir)
+	if err != nil {
+		return err
 	}
 
 	return nil
@@ -34,6 +50,7 @@ func Benchmark(b *testing.B) {
 	}
 
 	fmt.Printf("Using %q as the dataset\n", benchDir)
+	fmt.Printf("Using %q as the output\n", outDir)
 
 	b.Run("vanilla", func(b *testing.B) {
 		for b.Loop() {
@@ -45,15 +62,23 @@ func Benchmark(b *testing.B) {
 }
 
 func vanillaWalk() error {
+	outPath := filepath.Join(outDir, "walk-vanilla.txt")
+	f, err := os.Create(outPath)
+	if err != nil {
+		return fmt.Errorf("failed to create the output file %q. %w", outPath, err)
+	}
+	defer f.Close()
+
 	var fn fs.WalkDirFunc = func(path string, d fs.DirEntry, rcvErr error) error {
 		if rcvErr != nil {
 			return rcvErr
 		}
+		fmt.Fprintln(f, path)
 		return nil
 	}
 
 	w := file.NewWalker()
-	err := w.Walk(benchDir, fn)
+	err = w.Walk(benchDir, fn)
 	if err != nil {
 		return err
 	}
